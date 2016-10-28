c6ad142661988beff8e81b4f394a3c5145831782
Vetaart <m504708@yandex.ru>
2016-10-28 09:32:50 +0300

    д/з регистрация

---
+++ regFunction.php
@@ -0,0 +1,22 @@
+﻿<?
+// update records
+   function update_rec($email,$password){
+	   include('config.php');
+       mysql_connect($dbhost, $dbuser, $dbpasswd) or die("Couldn't establish connection");
+       mysql_select_db($dbname);
+       //echo "email='".$email."' ; password='".$password."' <br>";
+       $new_Pass=md5(strip_tags(addslashes(trim($password))));
+	   
+       if (isset($email))   {
+		  $q_select= "select* from list where email='".$email."'";
+		  $result = mysql_query($q_select);
+		  if (mysql_num_rows($result)>0) {
+	            $query = "UPDATE list SET  hpassword = '".$new_Pass."'  WHERE email='".$email."'";
+                mysql_query($query) or die("Ошибка запроса обновления!");
+                echo "Изменение пароля прошло успешно!";
+		                                   }
+		  else {echo "Такого пользователя нет! <br> Вам необходимо зарегистрироваться!";};
+               };
+		return $email;
+   };
+?>
