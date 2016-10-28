51157a926c0dad395e741c74e3f29f3a3afed477
Vetaart <m504708@yandex.ru>
2016-10-28 09:35:33 +0300

    д/з

---
+++ enterReg.php
@@ -0,0 +1,26 @@
+﻿<html>
+
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Актуализация</title>
+</head>
+<body>
+<form method="POST" action="enter.php">
+<p> Войдите под своим именем! </p>
+  <table border="0" width="47%">
+    <tr>    
+      <td width="34%">E-mail</td>
+      <td width="71%"><input type="text" name="id" size="36"></td>
+    </tr>
+	<tr>
+      <td width="34%">Введите Ваш пароль:</td>
+      <td width="71%"><input type="password" id="parolid" name="hpassword" size="36"  ></td>
+    </tr>	  
+  </table>
+  <p><input type="submit" value="Вход" name="enter"> <input type="submit" value="Регистрация" name="regnew"></p>
+</form>
+
+</body>
+
+</html> 
